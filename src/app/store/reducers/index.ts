5282a5ee9501b7e59ef392697314ab603f21b3a0
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2019-03-23 20:37:32 -0300

    Custom Router State Serializers

--- src/app/store/reducers/index.ts
+++ src/app/store/reducers/index.ts
@@ -1,5 +1,5 @@
 import { ActionReducerMap, createFeatureSelector } from '@ngrx/store';
-import { Params } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, Params } from '@angular/router';
 
 
 import * as fromRouter from '@ngrx/router-store';
@@ -21,4 +21,24 @@ export const reducers: ActionReducerMap<State> = {
 }
 
 export const getRouterState =
-		createFeatureSelector<fromRouter.RouterReducerState<RouterStateUrl>>('routerReducer');
+	createFeatureSelector<fromRouter.RouterReducerState<RouterStateUrl>>('routerReducer');
+		
+
+export class CustomSerializer implements fromRouter.RouterStateSerializer<RouterStateUrl> {
+	serialize(routerState: RouterStateSnapshot): RouterStateUrl {
+		// equivalent const url = routerState.url;
+		const { url } = routerState;
+		const { queryParams } = routerState.root;
+		
+		let state: ActivatedRouteSnapshot = routerState.root;
+
+		while (state.firstChild) {
+			state = state.firstChild;
+		}
+		const { params } = state;
+		
+	
+		return { url, queryParams, params  };
+	}
+
+}
