1a78a04a0ed90fe09c5d5ae1f74d831926f3682b
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2019-03-23 15:09:22 -0300

    State composition with Selectors

--- src/products/containers/products/products.component.ts
+++ src/products/containers/products/products.component.ts
@@ -1,7 +1,11 @@
+import { Observable } from 'rxjs/Observable';
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 
 import { Pizza } from '../../models/pizza.model';
-import { PizzasService } from '../../services/pizzas.service';
+
+import { Store } from '@ngrx/store';
+import * as fromStore from '../../store';
+
 
 @Component({
   selector: 'products',
@@ -16,11 +20,11 @@ import { PizzasService } from '../../services/pizzas.service';
         </a>
       </div>
       <div class="products__list">
-        <div *ngIf="!((pizzas)?.length)">
+        <div *ngIf="!((pizzas$ | async)?.length)">
           No pizzas, add one to get started.
         </div>
         <pizza-item
-          *ngFor="let pizza of (pizzas)"
+          *ngFor="let pizza of (pizzas$) | async"
           [pizza]="pizza">
         </pizza-item>
       </div>
@@ -29,12 +33,12 @@ import { PizzasService } from '../../services/pizzas.service';
 })
 export class ProductsComponent implements OnInit {
   pizzas: Pizza[];
+  pizzas$: Observable<Array<Pizza>>;
 
-  constructor(private pizzaService: PizzasService) {}
+  constructor(private store: Store<fromStore.ProductState>) {}
 
   ngOnInit() {
-    this.pizzaService.getPizzas().subscribe(pizzas => {
-      this.pizzas = pizzas;
-    });
+     // this.store.select<any>(fromStore.getAllPizzas).subscribe(state => this.pizzas = state);
+    this.pizzas$ = this.store.select<any>(fromStore.getAllPizzas);
   }
 }
--- src/products/store/reducers/index.ts
+++ src/products/store/reducers/index.ts
@@ -1,12 +1,33 @@
-import { ActionReducerMap } from '@ngrx/store';
+import { ActionReducerMap, createSelector, createFeatureSelector } from "@ngrx/store";
 
-import * as fromPizzas from './pizzas.reducer';
-
+import * as fromPizzas from "./pizzas.reducer";
 
 export interface ProductState {
-	pizzas: fromPizzas.PizzaState
+  pizzas: fromPizzas.PizzaState;
 }
 
 export const reducers: ActionReducerMap<ProductState> = {
-	pizzas: fromPizzas.reducer
-}
+  pizzas: fromPizzas.reducer
+};
+
+export const getProductsState = createFeatureSelector<ProductState>("products");
+
+
+// pizza state
+export const getPizzaState = createSelector(getProductsState, (state: ProductState) => state.pizzas);
+
+export const getAllPizzas = createSelector(getPizzaState, fromPizzas.getPizzas);
+export const getPizzasLoaded = createSelector(getPizzaState, fromPizzas.getPizzasLoaded);
+export const getPizzasLoading = createSelector(getPizzaState, fromPizzas.getPizzasLoading);
+
+// diminuiu o objeto (levels), criando seletores, por exemplo: 
+
+// const state = { // export const getPizzaState
+// 	products: {
+// 		pizzas: {
+// 			data: [],  // export const getAllPizzas
+// 			loaded: false, // export const getPizzasLoaded 
+// 			loading: false; // export const getPizzasLoading
+// 		}
+// 	}
+// }
